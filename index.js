c76f43476f601b0296b0114056a985c2624ff3f1
Kyle Housel <kylegranthousel@gmail.com>
2025-05-19 13:57:27 -0400

    Pass tests, look for refactor opportunities

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-// Your code here
+class Polygon {
+
+  constructor(sides) {
+    this.sides = sides
+  }
+
+  get countSides() {
+    return this.calcSides()
+  }
+
+  calcSides() {
+    return this.sides.length
+  }
+
+  get perimeter() {
+    return this.calcPerimeter()
+  }
+
+  calcPerimeter() {
+    return this.sides.reduce((acc, side) => acc + side)
+  }
+
+}
+
+class Triangle extends Polygon {
+
+  get isValid() {
+    return this.validityCheck()
+  }
+
+  validityCheck() {
+    if (this.countSides === 3 && this.sides[0] + this.sides[1] > this.sides[2] && this.sides[0] + this.sides[2] > this.sides[1] && this.sides[1] + this.sides[2] > this.sides[0]) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+class Square extends Polygon {
+
+  get isValid() {
+    return this.validityCheck()
+  }
+
+  validityCheck() {
+    if (this.sides[0] === this.sides[1] && this.sides[0] === this.sides[2] && this.sides[0] === this.sides[3]) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  get area() {
+    return this.calcArea()
+  }
+
+  calcArea() {
+    if (this.isValid) { return this.sides[0]**2 }
+  }
+
+}
